9b0999b3be843ef8b1243d7e90d36ebde3eaa042
Sub <maxim.k.elasnce@gmail.com>
2026-01-13 15:26:25 +0300

    script

--- markdown/README.md
+++ markdown/README.md
@@ -1,4 +1,62 @@
+[[README]]
 
+Можно определить несколько вариантов на одно действие.
+`case 0,2,3 => ...`
+### Пример с больше меньше
 ```
-pip install markdown
+let const x = input!(int);
+
+match(x)
+{
+	< 0 => println!("Меньше нуля");
+	0 => println!("Ноль");
+	> 0 => println!("Больше");
+}
+```
+
+
+### Пример с диапазоном
+```
+let const x = input!(int);
+
+match(x)
+{
+	[-10..0) => println!("1");
+	0 => println!("2");
+	[0..10], [20..30) => println!("3");
+	default => println!("4");
+}
+```
+
+| -10 | 1   |
+| --- | --- |
+| -5  | 1   |
+| 0   | 2   |
+| 5   | 3   |
+| 10  | 2   |
+| 20  | 3   |
+| 30  | 4   |
+
+### Пример с перечислением
+```
+enum TypesOfConnection
+{
+	TCP,
+	UDP,
+	HTTP,
+	SSH
+}
+
+
+
+int const t = TypesOfConnection::TCP;
+
+match(t)
+{
+	TCP => print("Tcp");
+	UDP => print("Udp");
+	HTTP => print("Http");
+	SSH => {connectSsh(); print("Ssh");}
+	default => assert::raise_error();
+}
 ```
--- markdown/main.py
+++ markdown/main.py
@@ -1,37 +1,77 @@
 import markdown
 import argparse
+from markdown.extensions.wikilinks import WikiLinkExtension
+from pathlib import Path
 
 class Converter():
+    fileType = "*.md"
     def __init__(self, folder = "."):
         self.folder = folder
 
     @staticmethod
     def getBasename(name):
         return name.removesuffix(".md")
-    
-    def addOverhead(self, htmlInner):
+    @staticmethod
+    def addOverhead(htmlInner):
         cssName = "theme.css"
         html = f"<head> <link rel=\"stylesheet\" href=\"{cssName}\"> </head>" + "<body>" + htmlInner + "</body>"
         return html
-    
-    def convertFile(self, filenameRaw):
-        filename = self.getBasename(filenameRaw)
-        with open(filename + ".md", "r") as mdF:
+    #def generateMenu(self):
+    def getFileStruct(self, directoryPath):
+        files = [f for f in Path(directoryPath).rglob(self.fileType) if f.is_file()]
+        for file in files:
+            print(file.relative_to(directoryPath))
+        fileByFolder = {}
+       
+        #for file in files:
+            
+        return files
+    def convertFile(self, filenameRaw, outputFilenameRaw):
+        filename = ""
+        if outputFilenameRaw is None:
+            filename = self.getBasename(filenameRaw)
+            outputFilenameRaw = filename + ".html"
+        
+        print(outputFilenameRaw)
+        with open(filenameRaw, "r", encoding='utf-8') as mdF:
             contentMD = mdF.read()
-            html = markdown.markdown(contentMD)
-            with open(filename + ".html", "w") as htmlF:
+            # Add filename as header
+            contentMD = f"# {filename}\n {contentMD}"
+            html = markdown.markdown(contentMD, extensions=[
+                "fenced_code",   # ```code blocks```
+                "tables",        # таблицы
+                "toc",           # оглавление
+                "codehilite",     # подсветка кода
+                "extra",
+                WikiLinkExtension(base_url='/docs/', end_url='.html')
+            ])
+            with open(outputFilenameRaw, "w", encoding='utf-8') as htmlF:
                 htmlF.write(self.addOverhead(html))
-    def convertAll(self):
-        self.convertFile("README.md")
+    def convertAll(self, dirPath, outPath):
+        files = self.getFileStruct(dirPath)
+        for file in files:
+            print(file)
+        pass
+    def convertOne(self, filename, outputFilename):
+        self.convertFile(filename, outputFilename)
+        #print("HEllo")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="MD to html")
     #parser.add_argument("-i", "--input", type=str, help="Input md file")
-    parser.add_argument("filename", help="Input md file")
+    parser.add_argument("-s", "--single", action='store_true', help="One file pass") # Store bool
+    parser.add_argument("-o", "--out", type=str, help="output file or folder")    
+    parser.add_argument("filename", help="Input md file or folder")
     args = parser.parse_args()
-    #print(f"{args.filename}")
+
+    #print(args.single)
+    #print(args.filename)
 
     converter = Converter()
-    converter.convertAll()
+    
+    if args.single:        
+        converter.convertOne(args.filename, args.out)
+    else:
+        converter.convertAll(args.filename, args.out)
 
     
