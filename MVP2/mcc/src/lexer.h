d3441dcd4a5856d01f08c4fbfbdc00ddd0a02ed6
Sub <11762109+sub-1337@users.noreply.github.com>
2026-07-05 13:34:01 +0300

    aaa

--- MVP2/mcc/src/lexer.h
+++ MVP2/mcc/src/lexer.h
@@ -1,12 +1,18 @@
 #pragma once
 #include <spdlog/spdlog.h>
 #include <string>
+#include <vector>
+#include <any>
 
 namespace mc
 {
 	enum class token_type
 	{
+		none,
 		teminal,
+		set,
+		colon, // двоеточие
+		semicolon, // точка с запятой
 		object,
 
 		open_bracket_round,
@@ -16,18 +22,56 @@ namespace mc
 		close_braket_square,
 
 		open_braket_qurly,
-		close_braket_qurly
-	};
-	class token
-	{
+		close_braket_qurly,
 
+		quotes_single,
+		quotes_double
 	};
+	enum class terminal
+	{
+		none,
+		let
+	};
+	using str = std::string;
+	using num = long long;
+	struct token
+	{
+		token_type type = token_type::none;
+		std::any value;
+		template<class T>
+		void setValue(token_type t, T val)
+		{
+			type = t,
+			value = val;
+		}
+		template<class T>
+		T getValue()
+		{
+			return std::any_cast<T>(value);
+		}
+	};
+	using token_stream = std::vector<token>;
+
 	class lexer
 	{
 	public:
-		void parse(const std::string& src)
+		token_stream parse(const std::string& src)
 		{
+			spdlog::set_level(spdlog::level::debug);
+			token_stream out;
 
+			for (int i = 0; i < src.length(); i++) 
+			{
+				auto it = str(1, src[i]);
+				token t;
+				t.setValue<str>(token_type::object, it);
+				//t.setValue(token_type::object, str(*it));
+				out.push_back(t);
+				//*it = std::toupper(*it);
+			}
+
+
+			return out;
 		}
 	};
 }
--- MVP2/mcc/tests/test_main.cpp
+++ MVP2/mcc/tests/test_main.cpp
@@ -1,9 +1,56 @@
 #include <catch2/catch_test_macros.hpp>
 #include "../src/lexer.h"
+#include <spdlog/spdlog.h>
 #include <string>
 
 TEST_CASE("basic test") {
     mc::lexer lex;
-    lex.parse("let a = 10;");
+    auto res = lex.parse("let a = 10;");
+    for (auto it : res)
+    {
+        spdlog::info(it.getValue<mc::str>());
+    }
     //REQUIRE(1 + 1 == 2);
+}
+
+TEST_CASE("basic value test") {
+    mc::token token;
+    token.setValue(mc::token_type::teminal, mc::terminal::none);
+    if (token.type == mc::token_type::teminal)
+    {
+        auto val = token.getValue<mc::terminal>();
+        REQUIRE(val == mc::terminal::none);
+    }
+    else
+    {
+        FAIL("error - wrong type has set");
+    }
+}
+
+TEST_CASE("basic value test 2") {
+    mc::token token;
+    token.setValue(mc::token_type::teminal, mc::terminal::let);
+    if (token.type == mc::token_type::teminal)
+    {
+        auto val = token.getValue<mc::terminal>();
+        REQUIRE(val == mc::terminal::let);
+    }
+    else
+    {
+        FAIL("error - wrong type has set");
+    }
+}
+
+TEST_CASE("basic value test 3") {
+    mc::token token;
+    token.setValue(mc::token_type::object, mc::str("a"));
+    if (token.type == mc::token_type::object)
+    {
+        auto val = token.getValue<mc::str>();
+        REQUIRE(val == mc::str("a"));
+    }
+    else
+    {
+        FAIL("error - wrong type has set");
+    }
 }
