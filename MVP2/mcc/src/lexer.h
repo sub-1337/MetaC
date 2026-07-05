4ac55c93ec66d5cf55f6e79c8794d4b60ec0232d
Sub <11762109+sub-1337@users.noreply.github.com>
2026-07-05 11:27:12 +0300

    sss

---
+++ MVP2/mcc/src/lexer.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <spdlog/spdlog.h>
+#include <string>
+
+class lexer
+{
+public:
+	void parse(const std::string& src)
+	{
+
+	}
+};
--- MVP2/mcc/tests/test_main.cpp
+++ MVP2/mcc/tests/test_main.cpp
@@ -1,5 +1,9 @@
 #include <catch2/catch_test_macros.hpp>
+#include "../src/lexer.h"
+#include <string>
 
 TEST_CASE("basic test") {
-    REQUIRE(1 + 1 == 2);
+    lexer lex;
+    lex.parse("let a = 10;");
+    //REQUIRE(1 + 1 == 2);
 }
